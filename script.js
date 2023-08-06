06adf554644ed2c35df25e76cb7b70b0c6bb8d2c
enayat789 <enayatbhai2006@gmail.com>
2023-08-06 18:26:44 +0530

    new commit

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // let text1 = document.getElementById("text").value;
 
+// let text5 ="";
 function showText() {
   let text1 = document.getElementById("text").value;
   // document.getElementById("display").innerHTML += text1;
-  // document.getElementById("display").style.display = "block";
 
   if (text1 === "") {
     alert("Enter task Name");
-    document.getElementById("display").style.display = "none";
+    // document.getElementById("display").style.display = "none";
   } else {
-    document.getElementById("display").innerHTML += text1;
+    document.getElementById("display").innerHTML += text1 + " <br> "; // its show the list one by one
     document.getElementById("display").style.display = "block";
+    document.getElementById("text").value = ""; // it clear the input fild
   }
-
-  let moreTask = [];
-  document.getElementById("display").innerHTML  = moreTask.push(text1);
 }
