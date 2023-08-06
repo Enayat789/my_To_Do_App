4f196e5f01433061e523a5348bb77da1fd21e996
enayat789 <enayatbhai2006@gmail.com>
2023-08-06 14:03:30 +0530

    created an array

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 
 function showText() {
   let text1 = document.getElementById("text").value;
-  document.getElementById("display").innerHTML = text1;
+  // document.getElementById("display").innerHTML += text1;
+  // document.getElementById("display").style.display = "block";
 
-  if (text1 == 0) {
+  if (text1 === "") {
     alert("Enter task Name");
+    document.getElementById("display").style.display = "none";
+  } else {
+    document.getElementById("display").innerHTML += text1;
+    document.getElementById("display").style.display = "block";
   }
+
+  let moreTask = [];
+  document.getElementById("display").innerHTML  = moreTask.push(text1);
 }
